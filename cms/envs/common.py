111ec26dde8256c36388286a87cddf738a3ca6eb
farhan <farhan.khan@arbisoft.com>
2026-02-24 17:45:07 +0500

    chore: tagging app fix

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -804,7 +804,7 @@ INSTALLED_APPS = [
     'statici18n',
 
     # Tagging
-    'cms.lib.xblock.tagging',
+    'cms.lib.xblock.tagging.apps.TaggingConfig',
 
     # Enables default site and redirects
     'django_sites_extensions',
